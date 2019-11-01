491f6715462639169889c74bf061dc9531c9baff
Kennehsq <kennedyndegwa@gmail.com>
2019-11-01 14:48:55 +0300

    Third Commit

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -86,13 +86,9 @@ public class MainActivity extends AppCompatActivity {
 
     //@Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
             Intent infoIntent = new Intent(MainActivity.this, InfoActivity.class);
             startActivity(infoIntent);
-
-
         return super.onOptionsItemSelected(item);
-
     }
 
     private void addCandiesToDatabase(Candy[] candies) {
